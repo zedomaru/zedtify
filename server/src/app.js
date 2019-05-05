7edd80f8b090a37f359959ca76701c256973e462
Zaki Elyasa Djauhari <zakielyasadjauhari@gmail.com>
2019-05-05 15:36:49 +0700

    server works properly

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,15 @@
-console.log("WA");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(morgan("combined"));
+app.use(cors());
+
+app.get("/status", (req, res) => {
+  res.json({ message: "hello world" });
+});
+
+app.listen(process.env.PORT || 3000);
