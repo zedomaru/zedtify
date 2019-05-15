9f0ab6fa4cb1b0fafea944e4417b535f38c90935
Zaki Elyasa Djauhari <zakielyasadjauhari@gmail.com>
2019-05-15 14:09:30 +0700

    fixing port, adding vuetify, finishing registration page

--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -1,14 +1,24 @@
 <template>
-  <div>
-    <h1>Register</h1>
-    <input type="email" name="email" placeholder="Email" v-model="email">
-    <br>
-    <input type="password" name="password" placeholder="Password" v-model="password">
-    <br>
-    <button @click="register">Register</button>
-    <!-- <p>{{email}}</p>
-    <p>{{password}}</p>-->
-  </div>
+  <v-layout column>
+    <v-flex xs6 offset-xs3>
+      <div class="white elevation-2">
+        <v-toolbar flat dense class="cyan" dark>
+          <v-toolbar-title>Register</v-toolbar-title>
+        </v-toolbar>
+        <div class="pl-4 pr-4 pt-2 pb-2">
+          <input type="email" name="email" placeholder="Email" v-model="email">
+          <br>
+          <input type="password" name="password" v-model="password" placeholder="Password">
+          <br>
+          <div v-html="error" class="error"></div>
+          <br>
+          <v-btn @click="register">Register</v-btn>
+          <!-- <p>{{email}}</p>
+          <p>{{password}}</p>-->
+        </div>
+      </div>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
@@ -17,25 +27,28 @@ export default {
   data() {
     return {
       email: "",
-      password: ""
+      password: "",
+      error: null
     };
   },
   methods: {
     async register() {
-      const response = await AuthenticationService.register({
-        email: this.email,
-        password: this.password
-      });
-      console.log(response.data);
+      try {
+        await AuthenticationService.register({
+          email: this.email,
+          password: this.password
+        });
+      } catch (error) {
+        this.error = error.response.data.error;
+        // console.log(error);
+      }
     }
   }
-  // watch: {
-  //   email(value) {
-  //     console.log("email changed ", value);
-  //   }
-  // }
 };
 </script>
 
-<style>
+<style scoped>
+.error {
+  color: red;
+}
 </style>
