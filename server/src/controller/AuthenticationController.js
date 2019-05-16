b56129200bc45295e5a229734b9e235584d60548
Zaki Elyasa Djauhari <zakielyasadjauhari@gmail.com>
2019-05-16 12:48:25 +0700

    login debugging and adding jwt

--- server/src/controller/AuthenticationController.js
+++ server/src/controller/AuthenticationController.js
@@ -1,14 +1,52 @@
 const { User } = require("../models");
+const jwt = require("jsonwebtoken");
+const config = require("../config/config");
+
+function jwtSignUser(user) {
+  const ONE_WEEK = 60 * 60 * 24 * 7;
+  return jwt.sign(user, config.authentication.jwtSecret, {
+    expiresIn: ONE_WEEK
+  });
+}
 
 module.exports = {
   async register(req, res) {
-    // res.send({ message: `Hello ${req.body.email}, welcome and have fun!` });
     try {
       const user = await User.create(req.body);
       res.send(user.toJSON());
-    } catch (error) {
+    } catch (err) {
       res.status(400).send({
         error: "MODOL KACA OI JANGAN DOBEL"
+      });
+    }
+  },
+  async login(req, res) {
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({
+        where: {
+          email: email
+        }
+      });
+      if (!user) {
+        return res.status(403).send({
+          error: "The login information was incorrect"
+        });
+      }
+      const isPasswordValid = password === user.password;
+      if (!isPasswordValid) {
+        return res.status(403).send({
+          error: "Login information was incorrect"
+        });
+      }
+      const userJSON = user.toJSON();
+      res.send({
+        user: userJSON,
+        token: jwtSignUser(userJSON)
+      });
+    } catch (err) {
+      res.status(500).send({
+        error: "An error occured"
       });
     }
   }
